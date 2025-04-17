5d89300a34de3b5d10e4d4e4b8e0f10b2f0f7616
Sean <sd2fsha627@gmail.com>
2025-04-17 14:36:19 +0800

    wip

--- client_sever_test/llm_client.py
+++ client_sever_test/llm_client.py
@@ -42,6 +42,32 @@ class MCPClient:
 
         stdio, write = stdio_transport
 
+        """
+        1. ClientSession(...) creates a session object that communicates over the stdio transport
+        2. It’s an async context manager, which:
+            - Does setup in __aenter__()
+            - Requires cleanup in __aexit__() (e.g. closing the connection)
+        """
         self.session = await self.exit_stack.enter_async_context(ClientSession(stdio, write))
 
         await self.session.initialize()
+
+    # interaction between user and mcp sever
+    async def process_query(self, query:str) -> str:
+        system_prompt = (
+            "You are a helpful assistant."
+            "You have the function of online search. "
+            "Please MUST call web_search tool to search the Internet content before answering."
+            "Please do not lose the user's question information when searching,"
+            "and try to maintain the completeness of the question content as much as possible."
+            "When there is a date related question in the user's question," 
+            "please use the search function directly to search and PROHIBIT inserting specific time."
+        )
+
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": query}
+        ]
+
+        # get all tools
+        response = await self.session.list_tools()
